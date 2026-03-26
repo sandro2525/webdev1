b59ba77f622ce0cedea2d491e497470c0b13c977
Lasha235 <gvatua.lasha1@gmail.com>
2026-03-26 18:35:05 +0400

    homework

--- myproject/project1.js
+++ myproject/project1.js
@@ -1,9 +1,9 @@
 const items = [
   { item: "Linen Overshirt", price: "$79.99" },
-  { item: "Margiela Gats", price: "$149.99" },
-  { item: "Levis jeans", price: "$89.99" },
-  { item: "Zara Jacket", price: "$119.99" },
-  { item: "Valentino", price: "$159.99" },
+  { item: "prada puffer jacket", price: "$149.99" },
+  { item: "valentino shoes", price: "$89.99" },
+  { item: "casio watch", price: "$119.99" },
+  { item: "margiela shirt", price: "$159.99" },
 ];
 
 const names = document.getElementsByClassName("product-name");
