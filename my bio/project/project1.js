dc52883be3a9b7530e243941c8c439edcc795c7d
sandro2525 <aleksandremachavariani804@gmail.com>
2026-05-07 17:48:42 +0400

    homework

---
+++ my bio/project/project1.js
@@ -0,0 +1,7 @@
+export const items = [
+  { item: "Linen Overshirt", price: "$79.99" },
+  { item: "Margiela Gats", price: "$119.99" },
+  { item: "Levis jeans", price: "$69.99" },
+  { item: "Zara Jacket", price: "$99.99" },
+  { item: "Valentino", price: "$189.99" },
+];
